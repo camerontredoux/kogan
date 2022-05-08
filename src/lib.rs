a4b4644856e3f27d4e298f5b0a127485a9fd49d5
Cameron Tredoux <camtredoux@gmail.com>
2022-05-08 16:36:54 -0700

    General command group added, setup command framework

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,24 @@
+use std::process;
+
 use serenity::{
     async_trait,
     client::{Context, EventHandler},
-    model::{gateway::Ready, interactions::Interaction},
+    framework::{
+        standard::{
+            macros::{command, group},
+            CommandResult,
+        },
+        Framework, StandardFramework,
+    },
+    http::Http,
+    model::{channel::Message, gateway::Ready, interactions::Interaction},
+    prelude::GatewayIntents,
+    Client,
 };
+
+#[group]
+#[commands(ping)]
+struct General;
 
 struct Handler;
 
@@ -15,5 +31,42 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 pub async fn init(token: String) {
-    println!("{}", token);
+    let intents = GatewayIntents::non_privileged()
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::GUILD_MEMBERS
+        | GatewayIntents::MESSAGE_CONTENT;
+    let http = Http::new(&token);
+    let bot_id = match http.get_current_application_info().await {
+        Ok(_) => match http.get_current_user().await {
+            Ok(bot_id) => bot_id.id,
+            Err(err) => {
+                println!("Failed to access bot id: {}", err);
+                process::exit(1);
+            }
+        },
+        Err(err) => {
+            println!("Failed to access bot info: {}", err);
+            process::exit(1);
+        }
+    };
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix(".").with_whitespace(true).on_mention(Some(bot_id)))
+        .group(&GENERAL_GROUP);
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    if let Err(err) = client.start().await {
+        println!("Failed to start client: {}", err);
+        process::exit(1);
+    };
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(&ctx.http, "content").await?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,5 @@ use kogan::*;
 
 fn main() {
     dotenv::dotenv().ok();
-    init(
-        std::env::var("TOKEN")
-            .expect("Missing token")
-            .parse()
-            .expect("Invalid token"),
-    );
+    init(std::env::var("TOKEN").expect("Missing TOKEN in .env"));
 }
