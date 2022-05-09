09102a623a55097644fe173cdac0f9c7f7c22f59
Cameron Tredoux <camtredoux@gmail.com>
2022-05-08 22:16:38 -0700

    Added subcommand for rules

--- src/lib.rs
+++ src/lib.rs
@@ -20,8 +20,15 @@ use serenity::{
 };
 
 #[group]
-#[commands(usage, rules, cat)]
+#[description = "General commands"]
+#[commands(usage, rules)]
 struct General;
+
+#[group]
+#[prefix = "emoji"]
+#[description = "Emoji messages"]
+#[commands(cat)]
+struct Emoji;
 
 struct Handler;
 
@@ -32,17 +39,20 @@ async fn delay_action(ctx: &Context, msg: &Message) {
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, ctx: Context, data: Ready) {}
+    async fn ready(&self, _ctx: Context, _: Ready) {
+        println!("Kōgan started and ready.");
+    }
 
-    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {}
+    async fn interaction_create(&self, _ctx: Context, _interaction: Interaction) {}
 }
 
 #[help]
-#[individual_command_tip = "If you want more information about a specific command, just pass the command as argument."]
+#[individual_command_tip = "If you want more information about a specific command, just pass the command as an argument. For example, .help rules"]
 #[command_not_found_text = "Could not find: `{}`."]
 #[max_levenshtein_distance(3)]
 #[lacking_permissions = "Hide"]
-#[strikethrough_commands_tip_in_guild("")]
+#[embed_success_colour("BLUE")]
+#[strikethrough_commands_tip_in_guild = ""]
 async fn help(
     context: &Context,
     msg: &Message,
@@ -84,6 +94,7 @@ pub async fn init(token: String) {
                 .limit_for(LimitedFor::User)
         })
         .await
+        .group(&EMOJI_GROUP)
         .group(&GENERAL_GROUP);
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
@@ -124,8 +135,11 @@ async fn usage(ctx: &Context, msg: &Message) -> CommandResult {
     Ok(())
 }
 
-#[command]
+#[command("rules")]
+#[sub_commands(bot)]
 #[description = "Send's the server and bot rules as a DM to the user."]
+#[example = "bot"]
+#[example = "server"]
 async fn rules(ctx: &Context, msg: &Message) -> CommandResult {
     let dm = msg
         .author
@@ -144,10 +158,52 @@ async fn rules(ctx: &Context, msg: &Message) -> CommandResult {
 }
 
 #[command]
+#[description = "Send's the bot rules as a DM to the user"]
+async fn bot(ctx: &Context, msg: &Message) -> CommandResult {
+    let dm = msg
+        .author
+        .dm(&ctx.http, |m| {
+            m.embed(|e| {
+                e.color(Color::ORANGE)
+                    .title("Kōgan Bot Rules")
+                    .description("Rules pertaining to the bot usage")
+                    .fields(vec![
+                        (
+                            "Rule 1",
+                            "Do not spam bot commands. I will be forced to implement a rate limiter, and I do not want to do that. Spamming commands will cause the bot to be blacklisted by the Discord API, and could cost me money depending on the host I use.",
+                            false,
+                        ),
+                        (
+                            "Rule 2",
+                            "Uhhh no other rules, enjoy the bot.",
+                            false
+                        )
+                    ])
+                    .footer(|f| {
+                        f.text("Created by Cameron Tredoux. Used on TLMBZ's server.")
+                    })
+            })
+        })
+        .await;
+
+    if let Err(err) = dm {
+        println!("Error DMing the author {}", err);
+    }
+
+    Ok(())
+}
+
+#[command]
 #[aliases("kitty", "neko")]
 #[bucket = "emoji"]
-async fn cat(ctx: &Context, msg: &Message) -> CommandResult {
+async fn cat(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     msg.channel_id.say(&ctx.http, ":cat:").await?;
+
+    if let Some(saying) = args.current() {
+        msg.channel_id
+            .say(&ctx.http, &format!("args: {}", saying))
+            .await?;
+    }
 
     Err(RevertBucket.into())
 }
